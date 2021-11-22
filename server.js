d2db4a229f014b32a77f33d5d8d5cee2a28e5cc9
Z4R4N06 <eddypdesouza@gmail.com>
2021-11-21 19:00:39 -0500

    user story

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 /* creating the route to request data */
+const fs = require('fs');
+const path = require('path');
 const { animals } = require('./data/animals');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
@@ -47,8 +49,34 @@ function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id ===id)[0];
     return result;
 }
-
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    // our function's main code will go here.
+    fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    // return finished code to post route for response
+    return animal;
+}
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
 // Creating routes.
+// req.query and req.params look for specific data that the server can send back to the client.
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -56,12 +84,30 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+// parse incoming JSON data
+app.use(express.json());
+
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     if (result) {
       res.json(result);
     } else {
       res.send(404);
+    }
+});
+// Post defines the route that listens for post requests from the client to the server
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be. 
+    req.body.id = animals.length.toString();
+    
+    // if any data in req.body is incorrect, send 400 error back.
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(req.body);
     }
 });
 
