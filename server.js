2843204889a9f079aa239f3479f59d686352a39e
Z4R4N06 <eddypdesouza@gmail.com>
2021-11-22 21:58:11 -0500

    front end integration

--- server.js
+++ server.js
@@ -43,10 +43,10 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
-} 
+}
 // Adding a new function.
 function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id ===id)[0];
+    const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
 }
 function createNewAnimal(body, animalsArray) {
@@ -54,8 +54,8 @@ function createNewAnimal(body, animalsArray) {
     animalsArray.push(animal);
     // our function's main code will go here.
     fs.writeFileSync(
-    path.join(__dirname, './data/animals.json'),
-    JSON.stringify({ animals: animalsArray }, null, 2)
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
     );
     // return finished code to post route for response
     return animal;
@@ -84,30 +84,48 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+app.use(express.static('public'));
 // parse incoming string or array data
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     if (result) {
-      res.json(result);
+        res.json(result);
     } else {
-      res.send(404);
+        res.send(404);
     }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 // Post defines the route that listens for post requests from the client to the server
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be. 
     req.body.id = animals.length.toString();
-    
+
     // if any data in req.body is incorrect, send 400 error back.
     if (!validateAnimal(req.body)) {
         res.status(400).send('The animal is not properly formatted.');
     } else {
-    const animal = createNewAnimal(req.body, animals);
-    res.json(req.body);
+        const animal = createNewAnimal(req.body, animals);
+        res.json(req.body);
     }
 });
 
